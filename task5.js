1e130a045ea716365834e3373fe722f6cb0e2452
Nguyen Duy Manh <manhduy0989@gmail.com>
2025-03-04 11:34:41 +0700

    baitapss20

--- task5.js
+++ task5.js
@@ -1,11 +1,11 @@
 let firstNumber= +prompt("Mời bạn nhập vào số đầu tiên");
 let secondNumber= +prompt("Mời bạn nhập vào số thứ hai");
-let tmp=firstNumber;
-if(!isNaN(firstNumber) && !isNaN(secondNumber)){
+let result=firstNumber;
+if(!isNaN(firstNumber) && !isNaN(secondNumber) && secondNumber>0){
     for(let i= 1; i<secondNumber;i++){
-        firstNumber*=tmp;
+        result*=firstNumber;
     }
-    alert(firstNumber);
+    alert(result);
 } else{
     alert('Không hợp lệ')
 }
